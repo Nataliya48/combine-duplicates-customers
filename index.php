374dcbe63fbd49edbb277b160894fa183fcb2127
nata <korovina-nataliya@mail.ru>
2019-04-09 18:06:37 +0300

    improvement

--- index.php
+++ index.php
@@ -12,14 +12,8 @@ $client = new \RetailCrm\ApiClient(
     \RetailCrm\ApiClient::V5
 );
 
-// получить список всех клиентов
-// записать в отдельный массив все E-mail и идентификатор клиента
-// следующим циклом выписать id всех клиентов, у которых E-mail совпадает больше 1 раза
-//
-
-// Получаем список всех клиентов
 try {
-    $response = $client->request->customersList([], null, 20);
+    $response = $client->request->customersList(['dateFrom' => '2016-01-01', 'dateTo' => '2017-12-31'], null, 20);
 } catch (\RetailCrm\Exception\CurlException $e) {
     echo "Connection error: " . $e->getMessage();
 }
@@ -29,29 +23,26 @@ if ($response->isSuccessful()) {
     $consolidatedClient = $response->customers[0];
     $clientList = [];
     for ($page = 1; $page <= $totalPageCount; $page++){
-        $responseCustomersList = $client->request->customersList(['maxOrdersCount' => 0, 'dateFrom' => '2016-01-01', 'dateTo' => '2017-12-31'], null, 20);
+        $responseCustomersList = $client->request->customersList(['dateFrom' => '2016-01-01', 'dateTo' => '2017-12-31'], $page, 20);
         foreach ($responseCustomersList->customers as $customer) {
             print_r($customer['email']);
             echo PHP_EOL;
             $emailList[$customer['id']] = $customer['email']; // Получаем список клиентов и их E-mail [id]=>[email]
+            //$customerList[$customer['id']] = $customer;
         }
     }
-    print_r($emailList);
-    echo PHP_EOL;
+    var_dump(count($emailList));
 
-    //$portions = array_chunk($clientList, 50, true);
-    /*foreach ($portions as $portion){
-        $responseСustomersCombine = $client->request->customersCombine($portion, $consolidatedClient);
-    }*/
-
-    $responseCustomersList2 = $client->request->customersList(['maxOrdersCount' => 0, 'dateFrom' => '2016-01-01', 'dateTo' => '2017-12-31'], $page, 20);
-    foreach ($responseCustomersList2->customers as $customer) {
-        $emailList = array_map(function ($value) use ($client, $customer) {
-            if ($value['email'] == $customer['email']) {
-                $responseСustomersCombine = $client->request->customersCombine($customer, $value);
-                var_dump($responseСustomersCombine);
+    for ($page = 1; $page <= $totalPageCount; $page++) {
+        $responseCustomersList2 = $client->request->customersList(['dateFrom' => '2016-01-01', 'dateTo' => '2017-12-31'], $page, 20);
+        foreach ($responseCustomersList2->customers as $customer) {
+            foreach ($emailList as $key => $email){
+                if ($email == $customer['email']) {
+                    $responseСustomersCombine = $client->request->customersCombine($customer, $customerList['необходимо передать тут массив']);
+                    var_dump($responseСustomersCombine);
+                }
             }
-        }, $emailList);
+        }
     }
 } else {
     echo sprintf(
